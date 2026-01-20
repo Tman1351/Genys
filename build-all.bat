b6ecd90c81dd8a5b00d3d8ba9865872ac9a0f23d
Tamanveer Dhillon <tdhillon009@gmail.com>
2026-01-19 20:56:51 -0800

    logging and assertion

--- build-all.bat
+++ build-all.bat
@@ -12,6 +12,6 @@ IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 PUSHD testbed
 CALL build.bat
 POPD
-IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
 ECHO "All assemblies built successfully."
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,18 @@
-#include <test.h>
+#include "../../engine/src/core/logger.h"
+#include "../../engine/src/core/asserts.h"
+
 
 int main(void){
-    print_int(24);
+    
+    GFATAL("A test message: %f", 4.32f);
+    GERROR("A test message: %f", 4.32f);
+    GWARN("A test message: %f", 4.32f);
+    GINFO("A test message: %f", 4.32f);
+    GDEBUG("A test message: %f", 4.32f);
+    GTRACE("A test message: %f", 4.32f);
+
+    GASSERT(1 == 0);
+
+
     return 0;
 }
